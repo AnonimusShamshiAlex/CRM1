3d7805e52a7325d2e4b35f6f12f0b63eaa4ff7df
AnonimusShamshiAlex <nackroot.uz@gmail.com>
2026-04-17 14:34:53 +0300

    Add missing ProjectMember model

---
+++ backend/src/models/ProjectMember.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const ProjectMember = sequelize.define('ProjectMember', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    projectId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'project_id',
+    },
+    userId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      field: 'user_id',
+    },
+    role: {
+      type: DataTypes.ENUM('manager', 'member', 'viewer'),
+      defaultValue: 'member',
+    },
+  }, {
+    tableName: 'project_members',
+    underscored: true,
+    timestamps: true,
+  });
+
+  return ProjectMember;
+};
